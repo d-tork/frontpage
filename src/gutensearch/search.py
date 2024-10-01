5137a6dc415253a48a7e0fdf3c0360cc826354de
dtork <dtork@dolores>
2024-10-01 09:34:47 -0400

    remove subparser for init
    It's better to just init as needed, less complex than having the user
    think about initializing their catalog.

--- src/gutensearch/search.py
+++ src/gutensearch/search.py
@@ -9,16 +9,20 @@ import pandas as pd
 import tqdm
 
 
-logging.basicConfig(level='INFO')
+logging.basicConfig(level='DEBUG')
 logger = logging.getLogger(__name__)
 
 
 def main():
     args = parse_args()
-    if args.query == 'init':
-        init_catalog()
+    logger.debug(f'Args: {args}')
+    try:
+        book_id = int(args.query)
+    except ValueError:
+        logger.info(f"Query '{args.query}' cannot be coerced to an integer ID; treating as word query")
+        query_word(args.query)
     else:
-        logger.info(f'Args: {args}')
+        query_book(book_id)
     return
 
 
@@ -26,17 +30,23 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Get word frequencies from a Project Gutenberg book.')
     parser.add_argument('query', help='PG book id or a search term')
     parser.add_argument('--limit', type=int, help='Number of search results to return')
-
-    # Subparser for management of database/cache
-    # subparser not actually used; this code generates the help message but ``query`` is checked for "init"
-    subparsers = parser.add_subparsers(title='subcommands', dest='subparser_name')
-    parser_init = subparsers.add_parser('init', help='Download catalog from Project Gutenberg')
     return parser.parse_args()
 
 
 def init_catalog():
     """Download or update the catalog."""
     logger.info('Initializing catalog')
+    return
+
+
+def query_book(id: int):
+    logger.info(f'Querying for book id {id}')
+    return
+
+
+def query_word(word: str):
+    logger.info(f'Querying for word "{word}"')
+    return
 
 
 if __name__ == '__main__':
